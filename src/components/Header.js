83fcc0450f7cea377582072c761f47fedd6c2f8b
Jenny Poon <jenny.poon@live.ca>
2020-10-07 22:19:18 -0700

    refactor: use functional react components for Header, Input and Weather Result; style: using material ui component library and minor styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,12 @@
-import React  from 'react';
+import React from "react";
 import "./Header.css";
 import sun from "./images/sun.png";
 
-class Header extends React.Component{
- render()
- {
- 	return(
-
- 			<div id="header">
- 			<p> Weather App</p><img src={sun} height="40px" width="40px" alt="logo"/>
- 			</div>
- 		)
- }
-
-}
+const Header = () => (
+  <div id='header'>
+    <p> Weather App</p>
+    <img src={sun} height='40px' width='40px' alt='logo' />
+  </div>
+);
 
 export default Header;
